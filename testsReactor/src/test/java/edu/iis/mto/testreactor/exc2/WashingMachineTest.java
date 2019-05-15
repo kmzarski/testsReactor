41cb9242bba0b5b351fa7653e066ab09413b4abb
Kamil Żarski <209526@edu.p.lodz.pl>
2019-05-15 17:31:46 +0200

    modify gitignore and start create mock

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -3,9 +3,25 @@ package edu.iis.mto.testreactor.exc2;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class WashingMachineTest {
+    @Mock
+    private DirtDetector dirtDetector;
+    @Mock
+    private Engine engine;
+    @Mock
+    private WaterPump waterPump;
+
+    @Before
+    public void init() {
+
+    }
 
     @Test
     public void itCompiles() {
