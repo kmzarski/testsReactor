640def467e375d18ef8e1c5bb86697dff4e83fd3
Kamil Żarski <209526@edu.p.lodz.pl>
2019-05-15 17:53:16 +0200

    seccond test, testing succes result

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -2,6 +2,8 @@ package edu.iis.mto.testreactor.exc2;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -22,12 +24,14 @@ public class WashingMachineTest {
     private WashingMachine washingMachine;
     private LaundryBatch laundryBatch;
     private ProgramConfiguration programConfiguration;
+    private Percentage percentage;
 
     @Before
     public void init() {
         washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
-       laundryBatch = LaundryBatch.builder().withWeightKg(1000).withType(Material.COTTON).build();
         programConfiguration = ProgramConfiguration.builder().withProgram(Program.AUTODETECT).withSpin(false).build();
+        percentage = new Percentage(3);
+        when(dirtDetector.detectDirtDegree(any())).thenReturn(percentage);
     }
 
     @Test
@@ -36,12 +40,27 @@ public class WashingMachineTest {
     }
 
     @Test
-    public void shouldReturnTooHeavy() {
-        LaundryStatus laundryStatus = washingMachine.start(laundryBatch, programConfiguration);
+    public void shouldReturnTooHeavyWhenIsOverload() {
+        laundryBatch = LaundryBatch.builder().withWeightKg(1000).withType(Material.COTTON).build();
         LaundryStatus expectedLaundryStatus = LaundryStatus.builder()
                 .withResult(Result.FAILURE)
                 .withErrorCode(ErrorCode.TOO_HEAVY)
                 .build();
+
+        LaundryStatus laundryStatus = washingMachine.start(laundryBatch, programConfiguration);
+
         assertThat(laundryStatus.getErrorCode(), is(expectedLaundryStatus.getErrorCode()));
+    }
+
+    @Test
+    public void shouldReturnSuccesWithNormalWeight() {
+        laundryBatch = LaundryBatch.builder().withWeightKg(6).withType(Material.COTTON).build();
+        LaundryStatus expectedLaundryStatus = LaundryStatus.builder()
+                .withResult(Result.SUCCESS)
+                .withRunnedProgram(Program.AUTODETECT)
+                .build();
+
+        LaundryStatus laundryStatus = washingMachine.start(laundryBatch, programConfiguration);
+        assertThat(laundryStatus.getResult(), is(expectedLaundryStatus.getResult()));
     }
 }
